e7e01ebc8dcfcfb8d174acef77b24b0db4a3b032
Winston Wacieni <winston.mureithi.i@gmail.com>
2023-02-16 11:59:34 +0300

    This is my first Commit

---
+++ object-creation.js
@@ -0,0 +1,14 @@
+// Creating classes
+// How to Create an Object in JavaScript
+// We'll use the code below as an example:
+
+const person = {
+  name: "Winston",
+  age: 30,
+  friends: ["ONeal", "Adele", "Chicago"],
+  greet: function () {
+    alert("This is an alert");
+  },
+};
+
+// end object creation //
