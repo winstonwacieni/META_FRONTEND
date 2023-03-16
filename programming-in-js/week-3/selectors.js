5f3c9bf0fd127852850b4ad44935febe4287052c
Winston Wacieni <winston.mureithi.i@gmail.com>
2023-03-16 11:03:20 +0300

    Selectors and DOM manipulation notes

---
+++ programming-in-js/week-3/selectors.js
@@ -0,0 +1,24 @@
+let answer = prompt("What is your name? ");
+
+//  output the typed-in information on the screen, as an <h1> HTML element.
+
+if (typeof answer === "string") {
+  var h1 = document.createElement("h1");
+  h1.innerHTML = answer;
+  document.body.innerText = "";
+  document.body.appendChild(h1);
+}
+
+var h1 = document.createElement("h1");
+h1.innerText = "Type into the input to make this text change";
+
+var input = document.createElement("input");
+input.setAttribute("type", "text");
+
+document.body.innerText = "";
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener("change", function () {
+  h1.innerText = input.value;
+});
