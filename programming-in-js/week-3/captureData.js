0ffbf4073f9995f04ae6306d8ec1dd9e73780e29
Winston Wacieni <winston.mureithi.i@gmail.com>
2023-03-16 14:01:28 +0300

    Jscript capture data Exercise

---
+++ programming-in-js/week-3/captureData.js
@@ -0,0 +1,14 @@
+switch (h1.innerText) {
+  case arr[0]:
+    h1.innerText = arr[1];
+    break;
+  case arr[1]:
+    h1.innerText = arr[2];
+    break;
+  case arr[2]:
+    h1.innerText = arr[3];
+    break;
+  default:
+    h1.innerText = arr[0];
+    break;
+}
